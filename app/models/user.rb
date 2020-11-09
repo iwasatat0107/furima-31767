a5f1f41b0f067f1ed23ebf1f4ff8a84ebdd53516
iwasatat0107 <it.was07@gmail.com>
2020-11-10 08:57:56 +0900

    RSpecの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,11 @@ class User < ApplicationRecord
     validates :last_name_kana, presence: true
     validates :first_name_kana, presence: true
   end
-    validates :nickname, presence: true
-    validates :birthday, presence: true
-    
-    validates :email, uniqueness: true
+  validates :nickname, presence: true
+  validates :birthday, presence: true
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates :email, uniqueness: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
