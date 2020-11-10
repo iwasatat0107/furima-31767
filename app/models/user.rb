733d4039ab7566d020f96b87c7e23ab2bc559b83
iwasatat0107 <it.was07@gmail.com>
2020-11-10 20:38:19 +0900

    User単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,11 @@ class User < ApplicationRecord
     validates :first_name_kana, presence: true
   end
   validates :nickname, presence: true
+  validates :encrypted_password, presence: true, length: { minimum: 7 }
   validates :birthday, presence: true
 
   validates :email, uniqueness: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  # validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              { 'abe' }
+    email                 { 'kkk@gmail.com' }
+    password              { '00000a' }
+
+    #  名前/カナ/生年月日
+    last_name            { '阿部' }
+    first_name           { '太郎' }
+    last_name_kana       { 'アベ' }
+    first_name_kana      { 'タロウ' }
+    birthday             { '1930-01-01' }
+  end
+end
