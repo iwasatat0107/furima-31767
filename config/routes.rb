41b13be9938334bb0a1e65499d5b2856a794bbe7
iwasatat0107 <it.was07@gmail.com>
2020-11-11 17:15:45 +0900

    "商品の詳細・配送について ActiveHashで作成""
    This reverts commit b8fe5ed3b6a2e9b4a98701d74a4f21226c7d4977.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :items #, only: :index
   #get 'items', to: 'items#index'
-  root  'items#index'
+  #root  'items#index'
+  root to: 'items#index'
 end
