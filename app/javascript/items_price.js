8c044604c09532b9b18555d62fe02895b55b38f1
iwasatat0107 <it.was07@gmail.com>
2020-11-12 19:05:46 +0900

    販売価格の表示

---
+++ app/javascript/items_price.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', () => {
+  console.log("OK");
+
+//価格入力欄のidを手がかりに変数にconstで代入する
+  const priceInput = document.getElementById("item-price");
+ 
+//価格のフォームに入力したあとにイベントを発火
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+
+//手数料
+  const addTaxDom = document.getElementById("add-tax-price");
+  const Tax = inputValue * 0.1
+        addTaxDom.innerHTML = Math.floor(Tax)
+
+//利益計算   
+  const saleFee  = document.getElementById("profit");
+        saleFee.innerHTML = Math.floor(inputValue - Tax)
+})
+
+});
+
