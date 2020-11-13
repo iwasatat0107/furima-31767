00ced6a6e5f0f71d97bb0470c04c5d56acfc9d04
iwasatat0107 <it.was07@gmail.com>
2020-11-13 13:57:57 +0900

    'priceが¥9,999,999より大きい場合には商品は出品できない'テスト追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -65,6 +65,11 @@ RSpec.describe Item, type: :model do
           @item.valid?
           expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
         end
+        it 'priceが9,999,999円以上だと商品は出品できない' do
+          @item.price = 100000000
+          @item.valid?
+          expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+        end
         it 'priceが半角数字意外だと商品は出品できない' do
           @item.price = '９９９'
           @item.valid?
